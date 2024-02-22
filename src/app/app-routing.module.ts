c92dccf97fa07e2961be0e443604738bab6c58e8
Abdallah <abdullah.taha574@gmail.com>
2024-02-22 14:24:59 +0200

    upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BlankLayoutComponent } from './components/blank-layout/blank-layout.component';
+import { AuthLayoutComponent } from './components/auth-layout/auth-layout.component';
+import { HomeComponent } from './components/home/home.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ProductsComponent } from './components/products/products.component';
+import { BrandsComponent } from './components/brands/brands.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { DetailsComponent } from './components/details/details.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+
+const routes: Routes = [
+
+  { path: '', component: BlankLayoutComponent, children: [
+    {path:'',redirectTo:'home',pathMatch:'full'},
+    {path:'home',component: HomeComponent},
+    {path:'cart',component: CartComponent},
+    {path:'products',component: ProductsComponent},
+    {path:'brands',component: BrandsComponent},
+    {path:'categories',component: CategoriesComponent},
+    {path:'details',component: DetailsComponent},
+  ]},
+
+  {path:'',component: AuthLayoutComponent, children:
+  [
+    {path:'login',component: LoginComponent},
+    {path:'register',component: RegisterComponent},
+  ]},
+
+
+  { path: '**', component: NotfoundComponent},
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
