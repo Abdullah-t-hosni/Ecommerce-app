1368d45cfa30ed6fd994d3a665d5d6c9212871ff
Abdallah <abdullah.taha574@gmail.com>
2024-02-22 14:44:09 +0200

    register & form validation

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -7,5 +7,25 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  registerForm: FormGroup = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20),
+    ]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^[A-Z][a-z0-9]{6,20}$/),
+    ]),
+    rePassword: new FormControl('' , [
+      Validators.required,
+      Validators.pattern(/^[A-Z][a-z0-9]{6,20}$/),
+    ]),
+    phone: new FormControl('' , [Validators.required , Validators.pattern(/^01[0125][0-9]{8}$/)]),
+  });
 
+  onSubmit() {
+    console.log(this.registerForm.value);
+  }
 }
