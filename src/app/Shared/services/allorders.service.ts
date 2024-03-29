389156f2d38b1f4c99dde84d22c13f5e6a576739
Abdallah <abdullah.taha574@gmail.com>
2024-03-29 16:03:54 +0200

    update

--- src/app/Shared/services/allorders.service.ts
+++ src/app/Shared/services/allorders.service.ts
@@ -24,12 +24,14 @@ export class AllordersService {
 
   getUserOrders(): Observable<any> {
     return this._HttpClient.get(
-      `https://ecommerce.routemisr.com/api/v1/orders/user/`
+      `https://ecommerce.routemisr.com/api/v1/orders/user/${this._AuthService.UserData.id}`
     );
   }
   getSingleOrderDetails(id: string | null): Observable<any> {
     return this._HttpClient.get(
       `https://ecommerce.routemisr.com/api/v1/orders/${id}`,
+
+      { headers: { token: this._CartService.userToken} }
     );
   }
 }
