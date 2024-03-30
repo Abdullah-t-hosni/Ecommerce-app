95c8cda2f88e7c0d26021e7ec7cb7655ef40768e
Noha M <33437197+NohaaAa@users.noreply.github.com>
2024-03-30 14:49:00 +0200

    Implement AuthService in OrdersComponent to get id and pass it to getUserOrders service

--- src/app/components/orders/orders.component.ts
+++ src/app/components/orders/orders.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Allorders } from 'src/app/Shared/interfaces/allorders';
+import {AuthService} from "../../Shared/services/auth.service";
 import { AllordersService } from 'src/app/Shared/services/allorders.service';
 
 @Component({
@@ -7,24 +8,26 @@ import { AllordersService } from 'src/app/Shared/services/allorders.service';
   templateUrl: './orders.component.html',
   styleUrls: ['./orders.component.css'],
 })
-export class OrdersComponent {
+export class OrdersComponent implements OnInit {
 
   truckCounter: number = 0;
   allOrders!: Allorders;
-  constructor(private _AllordersService: AllordersService) {}
+  constructor(private _AllordersService: AllordersService, private authService: AuthService) {}
 
   ngOnInit(): void {
     this.getUserOrders();
   }
 
   getUserOrders(): void {
-    this._AllordersService.getUserOrders().subscribe({
-      next: (response: Allorders) => {
-        this._AllordersService.changeTruckCount(response.length);
-        this.truckCounter = response.length;
-        this.allOrders = response;
-      },
-    });
+   this.authService.UserData.subscribe((data) => {
+     this._AllordersService.getUserOrders(data.id).subscribe({
+       next: (response: Allorders) => {
+         this._AllordersService.changeTruckCount(response.length);
+         this.truckCounter = response.length;
+         this.allOrders = response;
+       },
+     });
+   })
   }
 
 
